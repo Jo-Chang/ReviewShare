a72fc1c6b08e42bc42abf5acc1802e9740573be5
Jo-Chang <74034844+Jo-Chang@users.noreply.github.com>
2023-04-13 16:07:54 +0900

    Complete Topic-G / accounts - login, logout, signup, delete

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+
 
 # Create your views here.
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect('reviews:index')
+    else:
+        form = AuthenticationForm()
+        
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/login.html', context)
+
+
+def logout(request):
+    auth_logout(request)
+    return redirect('reviews:index')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('reviews:index')
+    else:
+        form = CustomUserCreationForm()
+        
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/signup.html', context)
+
+
+def delete(request):
+    request.user.delete()
+    auth_logout(request)
+    return redirect('reviews:index')
