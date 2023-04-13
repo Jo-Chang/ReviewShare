cc7e817c4d781b7d6d7a29800e6b5f97139295e4
SoozieKam <kam2301@naver.com>
2023-04-13 14:15:41 +0900

    Topic-C complete - reviews base, index, create, detail 작성

--- config/urls.py
+++ config/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from reviews import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('reviews/', include('reviews.urls')),
+    path('', views.redirect_index)
 ]
