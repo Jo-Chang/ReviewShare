f4a6f236ad162b095645056e6a8cd12ec86b77af
SoozieKam <kam2301@naver.com>
2023-04-13 16:36:58 +0900

    Topic-H complete comment  CRD

--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from .models import Review
-from .forms import ReviewForm
+from .models import Review, Comment
+from .forms import ReviewForm, CommentForm
 
 # Create your views here.
 
@@ -19,8 +19,12 @@ def redirect_index(request):
 
 def detail(request, review_pk):
     review = Review.objects.get(pk=review_pk)
+    comments = review.comment_set.all()
+    comment_form = CommentForm()
     context = {
         'review':review,
+        'comment_form': comment_form,
+        'comments':comments,
     }
     
     return render(request, 'reviews/detail.html', context)
@@ -62,3 +66,23 @@ def update(request, review_pk):
         'form': form,
     }
     return render(request, 'reviews/create.html', context)
+
+
+def comment_create(request, review_pk):
+    if request.method == 'POST':
+        review = Review.objects.get(pk=review_pk)
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.review = review
+            comment.save()
+            
+    return redirect('reviews:detail', review.pk)
+
+
+def comment_delete(request, review_pk, comment_pk):
+    comment = Comment.objects.get(pk=comment_pk)
+    comment.delete()
+    
+    return redirect('reviews:detail', review_pk)
+    
