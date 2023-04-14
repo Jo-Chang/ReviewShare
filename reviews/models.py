2e1348316a5fdedbd39915e982b9bf5ef7008f1d
SoozieKam <kam2301@naver.com>
2023-04-14 10:16:14 +0900

    complete rating

--- reviews/models.py
+++ reviews/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 class Review(models.Model):
-    title = models.CharField(max_length=20)
-    content = models.TextField()
-    movie = models.CharField(max_length=20)
-    
+    title = models.CharField('제목', max_length=20)
+    content = models.TextField('내용')
+    movie = models.CharField('영화', max_length=20)
+    score = models.PositiveIntegerField('평점', default=3, validators=[MinValueValidator(0), MaxValueValidator(5)])
 
 class Comment(models.Model):
     review = models.ForeignKey(to=Review, on_delete=models.CASCADE)
